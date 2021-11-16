31a481c64d66736155f7810619d327d852de228d
Tyler Weir <tylerw@live.com>
2021-11-16 09:58:58 -0800

    Game button style reacts to mouse interaction.

--- index.js
+++ index.js
@@ -5,27 +5,80 @@ var running = false;
 
 //request start
 //http://cs.pugetsound.edu/~dchiu/cs240/api/simone/?cmd=start
-// EVENT LISTENERS
 
+
+// EVENT LISTENERS
+// Red
 var redSq = document.getElementById("redSq");
 redSq.addEventListener("click", function() {
     console.log("Clicked Red");
 });
+redSq.addEventListener("mousedown", function() {
+    redSq.classList.add('lightred');
+});
+redSq.addEventListener("mouseup", function() {
+    redSq.classList.remove('lightred');
+});
+redSq.addEventListener("mouseover", function() {
+    redSq.classList.add('hover');
+})
+redSq.addEventListener("mouseleave", function() {
+    redSq.classList.remove('hover');
+})
 
+// Blue
 var blueSq = document.getElementById("blueSq");
 blueSq.addEventListener("click", function() {
     console.log("Clicked Blue");
 });
+blueSq.addEventListener("mousedown", function() {
+    blueSq.classList.add('lightblue');
+});
+blueSq.addEventListener("mouseup", function() {
+    blueSq.classList.remove('lightblue');
+});
+blueSq.addEventListener("mouseover", function() {
+    blueSq.classList.add('hover');
+})
+blueSq.addEventListener("mouseleave", function() {
+    blueSq.classList.remove('hover');
+})
 
+// Green
 var greenSq = document.getElementById("greenSq");
 greenSq.addEventListener("click", function() {
     console.log("Clicked Green");
 });
+greenSq.addEventListener("mousedown", function() {
+    greenSq.classList.add('lightgreen');
+});
+greenSq.addEventListener("mouseup", function() {
+    greenSq.classList.remove('lightgreen');
+});
+greenSq.addEventListener("mouseover", function() {
+    greenSq.classList.add('hover');
+})
+greenSq.addEventListener("mouseleave", function() {
+    greenSq.classList.remove('hover');
+})
 
+// Yellow
 var yellowSq = document.getElementById("yellowSq");
 yellowSq.addEventListener("click", function() {
     console.log("Clicked Yellow");
 });
+yellowSq.addEventListener("mousedown", function() {
+    yellowSq.classList.add('lightyellow');
+});
+yellowSq.addEventListener("mouseup", function() {
+    yellowSq.classList.remove('lightyellow');
+});
+yellowSq.addEventListener("mouseover", function() {
+    yellowSq.classList.add('hover');
+})
+yellowSq.addEventListener("mouseleave", function() {
+    yellowSq.classList.remove('hover');
+})
 
 var playBtn = document.getElementById("play");
 playBtn.addEventListener("click", function() {
