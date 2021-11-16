556fb96e15a7ed901fa13d0140c2448ec8dd465e
Tyler Weir <tylerw@live.com>
2021-11-16 09:45:07 -0800

    Made a successful API call.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const URL = "http://cs.pugetsound.edu/~dchiu/cs240/api/simone/"
+var running = false;
+//request solution
+//http://cs.pugetsound.edu/~dchiu/cs240/api/simone/?cmd=getSolution&rounds=4
 
-
+//request start
+//http://cs.pugetsound.edu/~dchiu/cs240/api/simone/?cmd=start
 // EVENT LISTENERS
+
 var redSq = document.getElementById("redSq");
 redSq.addEventListener("click", function() {
     console.log("Clicked Red");
@@ -26,4 +31,26 @@ var playBtn = document.getElementById("play");
 playBtn.addEventListener("click", function() {
     var numRounds = document.getElementById("rounds").value
     console.log("Lets play " + numRounds + " rounds of Simone");
+    getStartSequence();
 })
+
+
+function GameLoop(){
+    
+}
+
+async function getStartSequence(){
+    try {
+        let request = await axios.get("http://cs.pugetsound.edu/~dchiu/cs240/api/simone/?cmd=start");
+        console.log(request.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+/*
+ * Makes the game play the specified color sequence.
+ */
+function playStartSequence(sequence) {
+
+}
